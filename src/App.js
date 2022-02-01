58ff60c460b3c55608194f04509f10d50d90fc3f
Alina Dzhafarova <atlazdzhafarova69@gmail.com>
2022-02-01 14:04:19 +0200

    add react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import ReactDOM from 'react-dom';
 
-function App() {
+// Хуки
+
+
+  function App() {
+    const [name, setName] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <label> Введите Ваше имя:<br/>
+<input
+    value={name}
+    name="name"
+    onChange={(e) => setName(e.target.value)}
+/>
+        </label>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +33,33 @@ function App() {
   );
 }
 
+// Функциональный компонент
+//
+// function App() {
+//   function handleClick(props){
+//       console.log(props.target.value)
+//   }
+//
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo" />
+//                 <label> Введите Ваше имя:<br/>
+//                     <input
+//                         name="name"
+//                         onChange={handleClick}
+//                     />
+//                 </label>
+//                 <a
+//                     className="App-link"
+//                     href="https://reactjs.org"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     Learn React
+//                 </a>
+//             </header>
+//         </div>
+//     );
+// }
 export default App;
